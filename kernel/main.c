43e1613d098cfaf537a2a846e492acb90817bc5f
Francis Stokes <stokesie14@gmail.com>
2023-08-20 21:17:32 +0200

    Show OS logo at boot time

--- kernel/main.c
+++ kernel/main.c
@@ -6,21 +6,34 @@
 
 volatile u32 setup_complete = 0;
 
+void print_logo(void);
+
 // This is the first function executed in S-Mode.
 void main(void) {
   if (this_cpu_id() == 0) {
     uart_init();
     printf_init();
 
-    printf("Hello from cpu #%d\n", this_cpu_id());
+    print_logo();
+
 
     setup_complete = 1;
   } else {
     while (setup_complete == 0) {
       // Spin until system init complete
     }
-    printf("Hello from (non-zero) cpu #%d\n", this_cpu_id());
+    printf("Hart #%d online\n", this_cpu_id());
   }
 
   while (1) {}
+}
+
+void print_logo(void) {
+  printf(
+    ".---..                         .--.  .-. \n"
+    "|    |                        :    :(   )\n"
+    "|--- |  .-. .--..--. .-.  .--.|    | `-. \n"
+    "|    | (.-' `--.`--.(   ) |   :    ;(   )\n"
+    "'---'`- `--'`--'`--' `-'`-'    `--'  `-' \n"
+  );
 }
